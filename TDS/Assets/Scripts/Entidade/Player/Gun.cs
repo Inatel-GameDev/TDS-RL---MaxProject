a8525882ad2dc1b279cf34637771b1a2f48557b4
Aluno do Inatel <aluno@local.inatel.br>
2024-10-15 16:45:56 -0300

    Implementacao fireRate e Reload temporario

--- TDS/Assets/Scripts/Entidade/Player/Gun.cs
+++ TDS/Assets/Scripts/Entidade/Player/Gun.cs
@@ -9,11 +9,71 @@ public class Gun : MonoBehaviour
     public Transform firePoint;
     [Header("Variáveis")]
     public float fireForce = 20f;
+    public bool canFire = true;
+    public bool isReloading = false;
+    public int maxMag = 9;
+    public int currentMag = 9;
+
+    // fireDelay em segundos e fireRate em fixedGameUpdates
+    public float fireDelay = 0.2f;
+    private float fireRate = fireDelay * 50.0;
+
+    // Enquanto não houver animações
+    public float reloadDelay = 1.5f
+    private float reloadSpeed = reloadDelay * 50.0;
+
+    // Quantos fixedGameUpdates ocorreram desde um determinado evento
+    private int fUpdateCount = 0;
 
     public void Fire()
     {
-        GameObject bullet = Instantiate(bulletPreab, firePoint.position,firePoint.rotation);
-        bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.right * fireForce,ForceMode2D.Impulse);
+        if (canFire == true && isReloading == false &&  currentMag > 0)
+        {
+            GameObject bullet = Instantiate(bulletPreab, firePoint.position, firePoint.rotation);
+            bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.right * fireForce, ForceMode2D.Impulse);
+            canFire = false;
+        }
+        else if (isReloading == false && currentMag == 0)
+        {
+            Reload();
+        }
     }
 
+    public void Reload()
+    {
+        canFire = false;
+        isReloading = true;
+        fUpdateCount = 0;
+    }
+
+    void FixedUpdate()
+    {
+        // Implementação da fire rate
+        if(canFire == false && isReloading == false)
+        {
+            if(fUpdateCount<fireRate)
+            {
+                fUpdateCount++;
+            }
+            else
+            {
+                fUpdateCount = 0;
+                canFire = true;
+            }
+        }
+
+        // Implemetação temporária do reload
+        else if (canFire == false && isReloading == true)
+        {
+            if (fUpdateCount < reloadSpeed)
+            {
+                fUpdateCount++;
+            }
+            else
+            {
+                fUpdateCount = 0;
+                canFire = true;
+                isReloading == false;
+            }
+        }
 }
